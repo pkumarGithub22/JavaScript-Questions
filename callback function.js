cca8577c3e311ec4fcb12a62027b5fa471436c08
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-11-30 09:19:09 +0000

    changes in callback files

--- callback function.js
+++ callback function.js
@@ -1,6 +1,14 @@
 setTimeout(function () {
     console.log("5 second badd chal jana")
+    
 
+}, 5000);
+
+
+// add new code here 
+
+setInterval(() => {
+    console.log("hello puneet");
 }, 5000);
 
 // aisa code baad me chalta hai usse ek function de dete hai ki jab complete ho jana to ye function chla dena aur wo function jo hum dete hai 
