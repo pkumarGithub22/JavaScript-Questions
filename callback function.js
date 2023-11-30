6d4a26c625bcbbf44e9e67ffe7b4137898c052ce
Puneet Kumar <puneetgautam739@.com>
2023-11-30 12:45:47 +0530

    Callback Function

---
+++ callback function.js
@@ -0,0 +1,7 @@
+setTimeout(function () {
+    console.log("5 second badd chal jana")
+
+}, 5000);
+
+// aisa code baad me chalta hai usse ek function de dete hai ki jab complete ho jana to ye function chla dena aur wo function jo hum dete hai 
+// wo ek normal function hi hota hai usse khete hai callback function 
