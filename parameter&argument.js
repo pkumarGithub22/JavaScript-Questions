01cb7c1197c47e79a573afc6762e0e5fafc67972
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-02 17:33:52 +0000

    Parameter&Argument

---
+++ parameter&argument.js
@@ -0,0 +1,7 @@
+function abcd(a){
+ console.log(a);
+}abcd(12)// it is call parameter 
+
+//argument:- real value jo hum dete hai function chalate wqat
+
+//parameter:- jisme argument wali value store hoti hai 
