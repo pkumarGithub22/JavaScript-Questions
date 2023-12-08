e5d2dfe7cbf3abb41db0e05c236af5a73d964b4b
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-08 16:49:25 +0000

    What is lexical environment

---
+++ lexicaEnvironment.js
@@ -0,0 +1,9 @@
+// Theroy
+
+//                                      explain
+
+
+// 1) lexical environment hota hai el chart jisme ye linkha hota hai 
+// 2)  ke apka particular function kin chezo ko access karega kin ko nhi, matlb ki its hold scope and scope chain
+
+
