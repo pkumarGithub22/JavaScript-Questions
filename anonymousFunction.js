7bc75dd34e22abadb4331c1537a12fe5efb06d6a
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-11 17:21:45 +0000

    AnonymousFunction

---
+++ anonymousFunction.js
@@ -0,0 +1,11 @@
+//  ye ek aise function kon refer karta hai jiska moi name aur koi title na ho , js me anonymous function ek aise cheeze hai jisse bina kisi phechann ke declear kiya ja sakta hai 
+//  yeh ek distinction or anonymous function ke bech anther hai 
+
+//  yaha anonymous function banaye jane ke baad access nhi kya ja sakta 
+
+
+var test = function(){
+    console.log("This is an anonymous function");
+
+};
+test();
