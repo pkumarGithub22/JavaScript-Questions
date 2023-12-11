4e062230fb36ba2348b2cd52610dbecd098508d0
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-11 17:22:28 +0000

    Changes

--- callback function.js
+++ callback function.js
@@ -7,9 +7,9 @@ setTimeout(function () {
 
 // add new code here 
 
-setInterval(() => {
-    console.log("hello puneet");
-}, 5000);
+// setInterval(() => {
+//     console.log("hello puneet");
+// }, 5000);
 
 // aisa code baad me chalta hai usse ek function de dete hai ki jab complete ho jana to ye function chla dena aur wo function jo hum dete hai 
 // wo ek normal function hi hota hai usse khete hai callback function 
