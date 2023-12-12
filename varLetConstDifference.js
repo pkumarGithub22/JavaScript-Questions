2010cef634f72de5bd1bb0b6795f7833cbfb26c7
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-12 14:27:34 +0000

    var let const

---
+++ varLetConstDifference.js
@@ -0,0 +1,32 @@
+//    Var
+
+//   1) var Apne parent function me kahin bhi use ho sakta hai
+//   2) var old JS me tha 
+//   3) var Function Scoped hota hai
+//   4) var adds itself to the window object
+
+// function abcd(){
+//     for(var i= 1; i<12; i++){
+//         console.log(i);
+//     }
+//     console.log(i);
+
+// }
+// abcd();
+
+
+
+//                                          Let Const 
+
+// Let Const New js me hai 
+// let Braces scoped hota hai
+// let const does't add in window object
+
+ function ab(){
+    for(let i=0; i<12; i++){
+        console.log(i);
+
+    }
+    console.log(i)
+ }
+ ab();
