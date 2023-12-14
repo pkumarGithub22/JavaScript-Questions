388a1f817615574bd939c1df15901e52a123bb39
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-14 17:30:52 +0000

    Named Function

---
+++ NamedFunction.js
@@ -0,0 +1,6 @@
+                                     // Named Function
+ 
+ /*JS me named function keval ek function ko refer karne ka tarika hai,
+ jo function keyword ko ek name ke baad follow karta hai 
+jise us function ke callback ke roop me use kiya jata hai 
+*/ 
