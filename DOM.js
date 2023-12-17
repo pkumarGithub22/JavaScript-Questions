b4f0f9e25f0098cb815a445e60971bb46a4b8bf0
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-17 17:23:08 +0000

    What is DOM

---
+++ DOM.js
@@ -0,0 +1,8 @@
+                                    //DOM
+
+// The HTML DOM is a standard object model and programming interface for HTML. It defines:
+
+// The HTML elements as objects
+// The properties of all HTML elements
+// The methods to access all HTML elements
+// The events for all HTML elements
