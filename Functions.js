1cd1c9ef6f8fbd8ab711b304873763b2d10264c6
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-19 15:51:32 +0000

    What is function

---
+++ Functions.js
@@ -0,0 +1,11 @@
+// functions mainly teen kaam ke liye hote hai
+// 1) jab apka code aap turant nhi chalana chahte future me chalana chahte h0  
+// 2) jab apka code aap reuse karna chahte ho
+// 3) jab ap function chalana chahte ho har baar with the different data
+
+
+// note
+
+// functions ka matlb kuch code ko likhkar usko kuch name de sakte hai baad me use use kar sakte hai 
+
+// function = Code ko kuch name de dena 
