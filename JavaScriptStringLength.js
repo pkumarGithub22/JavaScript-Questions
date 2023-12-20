66bf869330ba085e9f0b23e562417a24c5d74708
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-20 15:34:56 +0000

    JavaScript String slice()

--- JavaScriptStringLength.js
+++ JavaScriptStringLength.js
@@ -5,3 +5,14 @@
 let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let length = text.length;
 console.log(length);
+
+                                       // JavaScript String slice()
+
+
+// slice() extracts a part of a string and returns the extracted part in a new string.
+// The method takes 2 parameters: start position, and end position (end not included).
+
+
+let text1 = "Apple, Banana, Kiwi";
+let part = text1.slice(15, 20);
+console.log(part);
