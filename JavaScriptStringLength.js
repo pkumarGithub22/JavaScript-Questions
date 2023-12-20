9ff6ff47551dbf3d2ff765ca623ef8371970f0f6
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-20 15:29:46 +0000

    JavaScript String Length

---
+++ JavaScriptStringLength.js
@@ -0,0 +1,7 @@
+// The length property returns the length of a string:
+
+// how to Check Length of String
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(length);
