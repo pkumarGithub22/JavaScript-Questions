e6b93854aea8b5709b0d2f83646c2a0daf3a5b64
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-22 16:02:01 +0000

    JavaScript Type Conversion, Converting Strings to Numbers

--- JavaScriptTypeConversion.js
+++ JavaScriptTypeConversion.js
@@ -2,3 +2,15 @@
 
 // 1)By the use of a JavaScript function
 // 2)Automatically by JavaScript itself
+
+
+//                 Converting Strings to Numbers 
+
+
+// The global method Number() converts a variable (or a value) into a number.
+
+// A numeric string (like "3.14") converts to a number (like 3.14).
+
+// An empty string (like "") converts to 0.
+
+// A non numeric string (like "John") converts to NaN (Not a Number).
