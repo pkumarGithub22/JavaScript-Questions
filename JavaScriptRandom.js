0f774830846a7a7c778cb7f7514c110eefe2a4d9
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-24 14:22:21 +0000

    JavaScript Random

--- JavaScriptRandom.js
+++ JavaScriptRandom.js
@@ -1,3 +1,8 @@
 // Math.random() returns a random number between 0,  and 1:
 
+// Math.random() always returns a number lower than 1.
+
+
+// Math.random() used with Math.floor() can be used to return random integers.
+
 Math.random();
