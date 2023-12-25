6d7551bf1783757e9909b8d29ec2e1d9c46585a2
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-25 16:50:09 +0000

    JavaScript Array Iteration

---
+++ JavaScriptArrayIteration.js
@@ -0,0 +1,10 @@
+// Array iteration methods operate on every array item.
+
+
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value, index, array) {
+  txt += value + "<br>";
+}
