9475121d9f896958b0a9f70f077adae117cf652e
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-26 16:33:39 +0000

    What is JavaScript Async

---
+++ JavaScriptAsync.js
@@ -0,0 +1,12 @@
+// "async and await make promises easier to write"
+// async makes a function return a Promise
+// await makes a function wait for a Promise
+
+
+
+// Async Syntax
+// The keyword async before a function makes the function return a promise:
+
+async function myFunction() {
+    return "Hello";
+  }
