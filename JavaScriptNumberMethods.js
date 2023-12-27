e8f5879b47036281d7ec3c42d3d67b2a067ed828
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-27 17:54:55 +0000

    JavaScript Number Methods

---
+++ JavaScriptNumberMethods.js
@@ -0,0 +1,11 @@
+// JavaScript Number Methods
+
+
+// These number methods can be used on all JavaScript numbers:
+
+// Method	Description
+// toString()	Returns a number as a string
+// toExponential()	Returns a number written in exponential notation
+// toFixed()	Returns a number written with a number of decimals
+// toPrecision()	Returns a number written with a specified length
+// ValueOf()	Returns a number as a number
