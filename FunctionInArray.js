70ae6218d186dfe8064edae7c80001e84ddc0c85
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-28 17:34:41 +0000

    Functions in array

---
+++ FunctionInArray.js
@@ -0,0 +1,10 @@
+// Push ==> push last in the Array
+
+// POP ==> Delete Value to the Last 
+
+// UnShift == > Remove Value To the array
+
+// Shift ==> Push to the First
+//  spilce => Push Value to the Middle 
+
+// Slice ==> Remove Value To the middle
