e126b3f5814ac236b54f5d9adf3421778e87534e
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2023-12-30 17:46:02 +0000

    Falsy And Truthy value in js

--- FalsyAndTruthy.js
+++ FalsyAndTruthy.js
@@ -1,3 +1,3 @@
-// Falsy value  ==>  0, null , undefined , false , NaN, document.all
+// 1) Falsy value  ==>  0, null , undefined , false , NaN, document.all
 
-//  Truthy Value ==> falsy value ko chorkar jitni value hoti hai wo truthy value hoti hai 
+//  2) Truthy Value ==> falsy value ko chorkar jitni value hoti hai wo truthy value hoti hai 
