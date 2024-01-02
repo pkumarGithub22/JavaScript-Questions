0dff35a94e5a78fadb2abf6b76bb5fab319915e0
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-01-02 17:43:32 +0000

    JavaScript Arrow Function

---
+++ JavaScriptArrowFunction.js
@@ -0,0 +1,15 @@
+// Arrow functions were introduced in ES6.
+
+// Arrow functions allow us to write shorter function syntax:
+
+// Before Arrow:
+hello = function() {
+  return "Hello World!";
+}
+
+// With Arrow Function:
+hello = () => {
+  return "Hello World!";
+}
+
+// It gets shorter! If the function has only one statement, and the statement returns a value, you can remove the brackets and the return keyword:
