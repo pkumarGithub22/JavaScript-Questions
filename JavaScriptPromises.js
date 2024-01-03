10a4c43260818de50df0af4f1c7374921c873940
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-01-03 18:04:02 +0000

    JavaScript Promises

---
+++ JavaScriptPromises.js
@@ -0,0 +1,13 @@
+// A Promise contains both the producing code and calls to the consuming code:
+let myPromise = new Promise(function(myResolve, myReject) {
+    // "Producing Code" (May take some time)
+    
+      myResolve(); // when successful
+      myReject();  // when error
+    });
+    
+    // "Consuming Code" (Must wait for a fulfilled Promise)
+    myPromise.then(
+      function(value) { /* code if successful */ },
+      function(error) { /* code if some error */ }
+    );
