29742f0673e3cb9d610fcb534577905f41cdd1a0
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-01-04 17:50:50 +0000

    What is the Coercion

--- Coercion.js
+++ Coercion.js
@@ -1,3 +1,3 @@
 // Ye ek automatic or implicit conversion hota hai  value from one data to another (string to number )
 //  jab value per different  type ke operator apply hote hai to case me automatic conversion ka behavior sur nhi hota hai  
-//  constructor of data type kisi value ko datatype me convert karne ke liye use hota hai like as number, string or boolean 
+//  constructor of data type kisi value ko datatype me convert karne ke liye use hota hai like as number, string or boolean. 
