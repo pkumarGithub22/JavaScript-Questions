37ae3657abae2dc7c638bb33956853225f04b9ea
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-01-08 18:11:13 +0000

    What is the Ajax

--- Ajax.js
+++ Ajax.js
@@ -3,3 +3,12 @@
 // Read data from a web server - after the page has loaded
 // Update a web page without reloading the page
 // Send data to a web server - in the background
+
+// AJAX = Asynchronous JavaScript And XML.
+
+// AJAX is not a programming language.
+
+// AJAX just uses a combination of:
+
+// A browser built-in XMLHttpRequest object (to request data from a web server)
+// JavaScript and HTML DOM (to display or use the data)
