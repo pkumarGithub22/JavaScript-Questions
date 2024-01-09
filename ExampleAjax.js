c40736a7335723ddceb584cbe4269c5ad855f3bd
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-01-09 16:41:15 +0000

    this example, we will see the basic use of async in JavaScript.

--- ExampleAjax.js
+++ ExampleAjax.js
@@ -4,3 +4,8 @@ const getData = async () => {
 }
  
 getData().then(data => console.log(data));
+
+// Async functions will always return a value.
+// It makes sure that a promise is returned and if 
+// it is not returned then JavaScript automatically wraps
+// it in a promise which is resolved with its value.
