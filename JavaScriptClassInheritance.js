a82d86e0b015bf0ab09903aba60c8eeb2661de0c
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-01-18 17:57:00 +0000

    Class Inheritance

---
+++ JavaScriptClassInheritance.js
@@ -0,0 +1,6 @@
+                                 // Class Inheritance
+
+
+// To create a class inheritance, use the extends keyword.
+
+// A class created with a class inheritance inherits all the methods from another class:
