8dbe1e73e2f26c15174533bba84e988f32dc9641
Puneet Kumar <100405505+pkumarGithub22@users.noreply.github.com>
2024-02-02 05:35:22 +0000

    What do you mean by Self Invoking Functions?

---
+++ SelfInvokingFunctions.js
@@ -0,0 +1,5 @@
+// Without being requested, a self-invoking expression is automatically invoked (initiated). 
+// If a function expression is followed by (), it will execute automatically. A function declaration cannot be invoked by itself.
+
+// Normally, we declare a function and call it, however, anonymous functions may be used to run a function automatically
+//  when it is described and will not be called again. And there is no name for these kinds of functions
